efcd01518b111ee48e86336b88916d1df9bed088
X01X012013 <1010535820@qq.com>
2016-01-13 18:16:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 window.onload = function(){
-  
-  
-}
+//Put in default prices
+$("#eyePrice").value(3.8);
+$("#TwicePrice").value(3.8);
+$("#eyePrice").value(3.8);
+$("#eyePrice").value(3.8);
+$("#eyePrice").value(3.8);
+
+return true;};
