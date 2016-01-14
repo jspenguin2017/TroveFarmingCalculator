b3b6d13ff754f334fe6426a6963dcfe51b96a1c0
X01X012013 <1010535820@qq.com>
2016-01-13 18:18:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.onload = function(){
 //Put in default prices
 $("#eyePrice").value(3.8);
-$("#TwicePrice").value(3.8);
-$("#eyePrice").value(3.8);
-$("#eyePrice").value(3.8);
-$("#eyePrice").value(3.8);
+$("#twicePrice").value(900);
+$("#thricePrice").value(3700);
+$("#quadPrice").value(13000);
+$("#pentaPrice").value(39000);
 
 return true;};
