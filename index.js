64301d937f8fafb93332030f77a3617d654cb3c2
X01X012013 <1010535820@qq.com>
2016-01-13 18:39:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
 window.onload = function(){
-//Put in default prices
+//Variables
 var defaultPrices = [3.8, 900, 3700, 13000, 39000];
+var isValid = [true, true, true, true, true];
+//On change validation
+var priceValidate = function(id, index){
+  var valueBuffer = parseFloat($(id).val());
+  $(id).removeClass("has-success has-warning has-error");
+  $(id + "Icon").removeClass("glyphicon-ok glyphicon-warning-sign glyphicon-remove")
+  if(valueBuffer === NaN || valueBuffer < 0){
+    //Not valid
+    $(id).addClass("has-error");
+    $(id + "Icon").addClass("glyphicon-remove");
+    isValid[index] = false;
+  }else if(valueBuffer < defaultPrices[index] / 2 || valueBuffer > defaultPrice[index] * 2){
+    //Warning: price far away from normal
+    $(id).addClass("has-warning");
+    $(id + "Icon").addClass("glyphicon-warning-sign");
+    isValid[index] = true;
+  }else{
+    //Passed all tests
+    $(id).addClass("has-success");
+    $(id + "Icon").addClass("glyphicon-ok");
+    isValid[index] = true;
+  }
+};
+$("#eyePrice").change(function(){
+    priceValidate("#eyePrice", 0)
+  });
+$("#twicePrice").change(function(){
+    priceValidate("#twicePrice", 1)
+  });
+$("#thricePrice").change(function(){
+    priceValidate("#thricePrice", 2)
+  });
+$("#quadPrice").change(function(){
+    priceValidate("#quadPrice", 3)
+  });
+$("#pentaPrice").change(function(){
+    priceValidate("#pentaPrice", 4)
+  });
+//Write in default prices
 $("#eyePrice").val(defaultPrices[0]);
 $("#twicePrice").val(defaultPrices[1]);
 $("#thricePrice").val(defaultPrices[2]);
 $("#quadPrice").val(defaultPrices[3]);
 $("#pentaPrice").val(defaultPrices[4]);
-//On change validation
-var priceValidate = function(id, index){
-  var valueBuffer = parseFloat($(id).val());
-  if(valueBuffer === NaN || valueBuffer < 0){
-    //Not valid
-    $(id).removeClass("has-success has-warning has-error");
-    $(id).addClass("has-error");
-  }
-}
-$("#eyePrice").change(function());
-$("#twicePrice").change(900);
-$("#thricePrice").change(3700);
-$("#quadPrice").change(13000);
-$("#pentaPrice").change(39000);
+//Function for "Calculate!" button
+var calculate = function(){
+  
+};
 return true;};
