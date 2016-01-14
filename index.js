a1d314231887fb4a235352f7e5cd1df7cd3d4e91
X01X012013 <1010535820@qq.com>
2016-01-13 19:12:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,7 @@
-window.onload = function(){
 //Variables
 var defaultPrices = [3.8, 900, 3700, 13000, 39000];
 var isValid = [true, true, true, true, true];
-//On change validation
-var priceValidate = function(id, index){
-  var valueBuffer = parseFloat($(id).val());
-  $(id + "Div").removeClass("has-success has-warning has-error");
-  $(id + "Icon").removeClass("glyphicon-ok glyphicon-warning-sign glyphicon-remove")
-  if(valueBuffer === NaN || valueBuffer < 0){
-    //Not valid
-    $(id  + "Div").addClass("has-error");
-    $(id + "Icon").addClass("glyphicon-remove");
-    isValid[index] = false;
-  }else if(valueBuffer < defaultPrices[index] / 2 || valueBuffer > defaultPrices[index] * 2){
-    //Warning: price far away from normal
-    $(id  + "Div").addClass("has-warning");
-    $(id + "Icon").addClass("glyphicon-warning-sign");
-    isValid[index] = true;
-  }else{
-    //Passed all tests
-    $(id  + "Div").addClass("has-success");
-    $(id + "Icon").addClass("glyphicon-ok");
-    isValid[index] = true;
-  }
-};
-$("#eyePrice").change(function(){
-    priceValidate("#eyePrice", 0)
-  });
-$("#twicePrice").change(function(){
-    priceValidate("#twicePrice", 1)
-  });
-$("#thricePrice").change(function(){
-    priceValidate("#thricePrice", 2)
-  });
-$("#quadPrice").change(function(){
-    priceValidate("#quadPrice", 3)
-  });
-$("#pentaPrice").change(function(){
-    priceValidate("#pentaPrice", 4)
-  });
-//Write in default prices
-$("#eyePrice").val(defaultPrices[0]);
-$("#twicePrice").val(defaultPrices[1]);
-$("#thricePrice").val(defaultPrices[2]);
-$("#quadPrice").val(defaultPrices[3]);
-$("#pentaPrice").val(defaultPrices[4]);
-//Database and kernal function
+//Database
 var Database = {
   //Deconstruction values, thanks to the author of: 
   //https://docs.google.com/spreadsheets/d/1G-9Fg8rGDKFV0zweJlWLKy1JLpbqf7pm6H7BxTX_PUc/edit#gid=1022828731
@@ -80,6 +36,29 @@ var Database = {
   fluxS44: 1500,
   fluxS45: 1700
 }
+//On change validation
+var priceValidate = function(id, index){
+  var valueBuffer = parseFloat($(id).val());
+  $(id + "Div").removeClass("has-success has-warning has-error");
+  $(id + "Icon").removeClass("glyphicon-ok glyphicon-warning-sign glyphicon-remove")
+  if(valueBuffer === NaN || valueBuffer < 0){
+    //Not valid
+    $(id  + "Div").addClass("has-error");
+    $(id + "Icon").addClass("glyphicon-remove");
+    isValid[index] = false;
+  }else if(valueBuffer < defaultPrices[index] / 2 || valueBuffer > defaultPrices[index] * 2){
+    //Warning: price far away from normal
+    $(id  + "Div").addClass("has-warning");
+    $(id + "Icon").addClass("glyphicon-warning-sign");
+    isValid[index] = true;
+  }else{
+    //Passed all tests
+    $(id  + "Div").addClass("has-success");
+    $(id + "Icon").addClass("glyphicon-ok");
+    isValid[index] = true;
+  }
+};
+//Kernal
 var calculate = function(){
   //If prices are not valid
   if(!(isValid[0] && isValid[1] && isValid[2] && isValid[3] && isValid[4])){
@@ -89,8 +68,32 @@ var calculate = function(){
   //Prices are valid, start calculating
   
 };
-$("#theButton").click(function(){
-  $("#ourputResultsDiv").css("display", "inline");
-  calculate();
-});
-return true;};
+//Init
+window.onload = function(){
+  $("#eyePrice").change(function(){
+      priceValidate("#eyePrice", 0)
+    });
+  $("#twicePrice").change(function(){
+      priceValidate("#twicePrice", 1)
+    });
+  $("#thricePrice").change(function(){
+      priceValidate("#thricePrice", 2)
+    });
+  $("#quadPrice").change(function(){
+      priceValidate("#quadPrice", 3)
+    });
+  $("#pentaPrice").change(function(){
+      priceValidate("#pentaPrice", 4)
+    });
+  //Write in default prices
+  $("#eyePrice").val(defaultPrices[0]);
+  $("#twicePrice").val(defaultPrices[1]);
+  $("#thricePrice").val(defaultPrices[2]);
+  $("#quadPrice").val(defaultPrices[3]);
+  $("#pentaPrice").val(defaultPrices[4]);
+  //Calculate! button
+  $("#theButton").click(function(){
+    $("#ourputResultsDiv").css("display", "inline");
+    calculate();
+  });
+};
