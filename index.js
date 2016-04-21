cd4165ff48b1f145e3650088d3b46199b69d6236
X01X012013 <1010535820@qq.com>
2016-04-21 17:29:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Initialize variables
 var names = ["Eye", "Twice", "Thrice", "Quad", "Penta", "Flame", "Radiant Soul"];
-var prices = [3, 400, 2700, 9500, 33000, 2300, 120000]; //Default prices
+var prices = [3, 175, 2000, 8500, 27000, 2300, 100000]; //Default prices
 var colors = {green: "#008000", yellow: "#B36B00", red: "#993333"};
 var priceRows = [];
 var priceRowFeedback = {
