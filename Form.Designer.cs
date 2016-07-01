868f0b49efb3be5c048f01a9a3fee5e59590c646
X01X012013 <1010535820@qq.com>
2016-06-30 19:08:42 -0600

    Add files via upload

---
+++ Form.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace ChallengeReminder
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.TrayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.TrayShowBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.TrayExitBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.TrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.LabelStartTime = new System.Windows.Forms.Label();
+            this.LabelShowTime = new System.Windows.Forms.Label();
+            this.TxtStartTime = new System.Windows.Forms.TextBox();
+            this.TxtShowTime = new System.Windows.Forms.TextBox();
+            this.LabelMsg = new System.Windows.Forms.Label();
+            this.TxtMsg = new System.Windows.Forms.TextBox();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.BtnSaveHide = new System.Windows.Forms.Button();
+            this.BtnTest = new System.Windows.Forms.Button();
+            this.BtnHelp = new System.Windows.Forms.Button();
+            this.MainTimer = new System.Windows.Forms.Timer(this.components);
+            this.TrayMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TrayMenu
+            // 
+            this.TrayMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TrayShowBtn,
+            this.toolStripSeparator1,
+            this.TrayExitBtn});
+            this.TrayMenu.Name = "TrayMenu";
+            this.TrayMenu.Size = new System.Drawing.Size(108, 54);
+            // 
+            // TrayShowBtn
+            // 
+            this.TrayShowBtn.Name = "TrayShowBtn";
+            this.TrayShowBtn.Size = new System.Drawing.Size(107, 22);
+            this.TrayShowBtn.Text = "Show";
+            this.TrayShowBtn.Click += new System.EventHandler(this.TrayShowBtn_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(104, 6);
+            // 
+            // TrayExitBtn
+            // 
+            this.TrayExitBtn.Name = "TrayExitBtn";
+            this.TrayExitBtn.Size = new System.Drawing.Size(107, 22);
+            this.TrayExitBtn.Text = "Exit";
+            this.TrayExitBtn.Click += new System.EventHandler(this.TrayExitBtn_Click);
+            // 
+            // TrayIcon
+            // 
+            this.TrayIcon.ContextMenuStrip = this.TrayMenu;
+            this.TrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("TrayIcon.Icon")));
+            this.TrayIcon.Text = "Challenge Reminder";
+            this.TrayIcon.Visible = true;
+            this.TrayIcon.DoubleClick += new System.EventHandler(this.TrayShowBtn_Click);
+            // 
+            // LabelStartTime
+            // 
+            this.LabelStartTime.AutoSize = true;
+            this.LabelStartTime.Location = new System.Drawing.Point(12, 15);
+            this.LabelStartTime.Name = "LabelStartTime";
+            this.LabelStartTime.Size = new System.Drawing.Size(137, 12);
+            this.LabelStartTime.TabIndex = 1;
+            this.LabelStartTime.Text = "Challenge Start Time: ";
+            // 
+            // LabelShowTime
+            // 
+            this.LabelShowTime.AutoSize = true;
+            this.LabelShowTime.Location = new System.Drawing.Point(12, 47);
+            this.LabelShowTime.Name = "LabelShowTime";
+            this.LabelShowTime.Size = new System.Drawing.Size(119, 12);
+            this.LabelShowTime.TabIndex = 2;
+            this.LabelShowTime.Text = "Show Time (in ms): ";
+            // 
+            // TxtStartTime
+            // 
+            this.TxtStartTime.Location = new System.Drawing.Point(155, 12);
+            this.TxtStartTime.Name = "TxtStartTime";
+            this.TxtStartTime.Size = new System.Drawing.Size(217, 21);
+            this.TxtStartTime.TabIndex = 3;
+            this.TxtStartTime.Text = "00";
+            // 
+            // TxtShowTime
+            // 
+            this.TxtShowTime.Location = new System.Drawing.Point(155, 44);
+            this.TxtShowTime.Name = "TxtShowTime";
+            this.TxtShowTime.Size = new System.Drawing.Size(217, 21);
+            this.TxtShowTime.TabIndex = 4;
+            this.TxtShowTime.Text = "5000";
+            // 
+            // LabelMsg
+            // 
+            this.LabelMsg.AutoSize = true;
+            this.LabelMsg.Location = new System.Drawing.Point(12, 78);
+            this.LabelMsg.Name = "LabelMsg";
+            this.LabelMsg.Size = new System.Drawing.Size(59, 12);
+            this.LabelMsg.TabIndex = 5;
+            this.LabelMsg.Text = "Message: ";
+            // 
+            // TxtMsg
+            // 
+            this.TxtMsg.Location = new System.Drawing.Point(12, 93);
+            this.TxtMsg.Multiline = true;
+            this.TxtMsg.Name = "TxtMsg";
+            this.TxtMsg.Size = new System.Drawing.Size(360, 111);
+            this.TxtMsg.TabIndex = 6;
+            this.TxtMsg.Text = "Challenge Time! \r\nGO GO GO! ";
+            this.TxtMsg.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtMsg_KeyDown);
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Location = new System.Drawing.Point(12, 210);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(75, 39);
+            this.BtnSave.TabIndex = 7;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // BtnSaveHide
+            // 
+            this.BtnSaveHide.Location = new System.Drawing.Point(93, 210);
+            this.BtnSaveHide.Name = "BtnSaveHide";
+            this.BtnSaveHide.Size = new System.Drawing.Size(117, 39);
+            this.BtnSaveHide.TabIndex = 8;
+            this.BtnSaveHide.Text = "Save And Hide";
+            this.BtnSaveHide.UseVisualStyleBackColor = true;
+            this.BtnSaveHide.Click += new System.EventHandler(this.BtnSaveHide_Click);
+            // 
+            // BtnTest
+            // 
+            this.BtnTest.Location = new System.Drawing.Point(216, 210);
+            this.BtnTest.Name = "BtnTest";
+            this.BtnTest.Size = new System.Drawing.Size(75, 39);
+            this.BtnTest.TabIndex = 9;
+            this.BtnTest.Text = "Test";
+            this.BtnTest.UseVisualStyleBackColor = true;
+            this.BtnTest.Click += new System.EventHandler(this.BtnTest_Click);
+            // 
+            // BtnHelp
+            // 
+            this.BtnHelp.Location = new System.Drawing.Point(297, 210);
+            this.BtnHelp.Name = "BtnHelp";
+            this.BtnHelp.Size = new System.Drawing.Size(75, 39);
+            this.BtnHelp.TabIndex = 10;
+            this.BtnHelp.Text = "Help";
+            this.BtnHelp.UseVisualStyleBackColor = true;
+            this.BtnHelp.Click += new System.EventHandler(this.BtnHelp_Click);
+            // 
+            // MainTimer
+            // 
+            this.MainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.BtnHelp);
+            this.Controls.Add(this.BtnTest);
+            this.Controls.Add(this.BtnSaveHide);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.TxtMsg);
+            this.Controls.Add(this.LabelMsg);
+            this.Controls.Add(this.TxtShowTime);
+            this.Controls.Add(this.TxtStartTime);
+            this.Controls.Add(this.LabelShowTime);
+            this.Controls.Add(this.LabelStartTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Challenge Reminder Settings";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form_Shown);
+            this.TrayMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip TrayMenu;
+        private System.Windows.Forms.ToolStripMenuItem TrayShowBtn;
+        private System.Windows.Forms.ToolStripMenuItem TrayExitBtn;
+        private System.Windows.Forms.NotifyIcon TrayIcon;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Label LabelStartTime;
+        private System.Windows.Forms.Label LabelShowTime;
+        private System.Windows.Forms.TextBox TxtStartTime;
+        private System.Windows.Forms.TextBox TxtShowTime;
+        private System.Windows.Forms.Label LabelMsg;
+        private System.Windows.Forms.TextBox TxtMsg;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnSaveHide;
+        private System.Windows.Forms.Button BtnTest;
+        private System.Windows.Forms.Button BtnHelp;
+        private System.Windows.Forms.Timer MainTimer;
+    }
+}
+
---
+++ Form.cs
@@ -0,0 +1,239 @@
+﻿/*
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+*/
+using System;
+using System.Timers;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+//TODO: 
+//Add core timing code, refresh after changing settings?
+//Add enabled to context menu
+//Some add to autostart function
+
+namespace ChallengeReminder
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        //=====Variables
+        //The minute of the hour when challenge start
+        private int startTime;
+        //The amount of milliseconds that the notification will be shown
+        private int showTime;
+        //The message to display
+        private string msg;
+        //If it is the timer's first tick
+        //On the first tick, the timer will adjust interval to 1 hour
+        private bool isFirstTick;
+
+        //=====Form Events=====
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Form load
+            //Set textboxes length limit
+            TxtStartTime.MaxLength = 2;
+            TxtShowTime.MaxLength = 6;
+            //Load settings
+            startTime = (int)Properties.Settings.Default["startTime"];
+            showTime = (int)Properties.Settings.Default["showTime"];
+            msg = (string)Properties.Settings.Default["msg"];
+            //Write settings into textboxes
+            TxtStartTime.Text = startTime.ToString();
+            TxtShowTime.Text = showTime.ToString();
+            TxtMsg.Text = msg;
+            //Schedule notification
+            scheduleNotification();
+        }
+        private void Form_Shown(object sender, EventArgs e)
+        {
+            //Form loaded
+            this.Hide();
+        }
+
+        //=====Main Mechanism=====
+        private void scheduleNotification()
+        {
+            //Schedule the first tick, subsequent ticks are set inside tick handler
+            int delay, sec, min;
+            //This will be accurate to one second, which is good enough
+            //Calculate the amount of seconds until next minute
+            sec = 60 - DateTime.Now.Second;
+            //Calculate the amount of minutes from the next minute until next challenge
+            if (DateTime.Now.Minute < startTime)
+            {
+                //The challenge is coming in this hour
+                //Minus one because we are calculating starting the next minute
+                min = startTime - DateTime.Now.Minute - 1;
+            }
+            else
+            {
+                //We need to calculate how many minutes are remaining in thie hour and add start time
+                //Using 59 instead of 60 because we are calculating starting the next minute
+                min = 59 - DateTime.Now.Minute + startTime;
+            }
+            //Put minutes and seconds into milliseconds
+            delay = (min * 60 + sec) * 1000;
+            //Just in case we are right on the hour
+            if(delay == 0)
+            {
+                //Set delay to an hour and display message
+                delay = 3600000;
+                displayMsg(showTime, msg);
+            }
+            //Set interval and reset timmer
+            isFirstTick = true;
+            MainTimer.Interval = delay;
+            MainTimer.Enabled = true;
+        }
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            //Set interval to 1 hour
+            if (isFirstTick)
+            {
+                //This is runned once per hour, it probably will not cause efficiency issues
+                MainTimer.Interval = 3600000;
+                isFirstTick = false;
+            }
+            //Display message
+            displayMsg(showTime, msg);
+        }
+
+        //=====Tray Icon Events=====
+        private void TrayShowBtn_Click(object sender, EventArgs e)
+        {
+            //"Show" menu item is clicked or tray icon is double-clicked
+            //Show the window
+            this.Show();
+            //Restore the window
+            this.WindowState = FormWindowState.Normal;
+        }
+        private void TrayExitBtn_Click(object sender, EventArgs e)
+        {
+            //"Exit" button is clicked
+            //Exit the application
+            Application.Exit();
+        }
+
+        //=====Buttons Events=====
+        private void BtnHelp_Click(object sender, EventArgs e)
+        {
+            //Help button clicked
+            //Show help message
+            MessageBox.Show(
+                "Challenge Start Time: The minute of the hour when the Challenge start, usually 0 or 30. The default value is 0. " + Environment.NewLine + Environment.NewLine +
+                "Show Time: The amount of milliseconds that the notification will display. Minimum and maximum limit depend on your system, some system completely ignores this settings. There are 1000 milliseconds in 1 second. The default value is 5000 (5 seconds). " + Environment.NewLine + Environment.NewLine +
+                "Message: The message to display. The length limit depends on your system. Click Test to see how it looks. " + Environment.NewLine + Environment.NewLine +
+                "Your settings will not take effect until you press Save or Save And Hide. Clicking close button or right click tray icon then click Exit to exit the application. ",
+                "Challenge Reminder Help"
+            );
+        }
+        private void BtnTest_Click(object sender, EventArgs e)
+        {
+            //Test button clicked
+            //Validate show time textbox
+            int time;
+            if (int.TryParse(TxtShowTime.Text, out time))
+            {
+                //Valid, show the message
+               displayMsg(time, TxtMsg.Text);
+            }
+            else
+            {
+                //Not valid
+                showError("Show Time", TxtShowTime);
+            }
+        }
+        private void BtnSaveHide_Click(object sender, EventArgs e)
+        {
+            //Save and hide button clicked
+            //Save settings and if succeed, hide main window
+            if (saveSettings())
+            {
+                this.Hide();
+            }
+        }
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            //Save button clicked
+            //Save settings
+            saveSettings();
+        }
+        private bool saveSettings()
+        {
+            //Validate settings
+            int i;
+            //Check start time
+            if (int.TryParse(TxtStartTime.Text, out i))
+            {
+                //Start time valid, write to variable
+                startTime = i;
+            }
+            else
+            {
+                //Start time not valid
+                showError("Start Time", TxtStartTime);
+                return false;
+            }
+            //Check show time
+            if (int.TryParse(TxtShowTime.Text, out i))
+            {
+                //Show time valid, write to variable
+                showTime = i;
+            }
+            else
+            {
+                //Show time not valid
+                showError("Show Time", TxtShowTime);
+                return false;
+            }
+            //Copy message to msg variable
+            msg = TxtMsg.Text;
+            //Save settings for next time
+            Properties.Settings.Default["startTime"] = startTime;
+            Properties.Settings.Default["showTime"] = showTime;
+            Properties.Settings.Default["msg"] = msg;
+            Properties.Settings.Default.Save();
+            //Re-schedule notification
+            scheduleNotification();
+            return true;
+        }
+        private void showError(string msg, TextBox t)
+        {
+            //Show an error message and focus the textbox
+            MessageBox.Show("Error: " + msg + " is not valid, your settings are not saved. ", "Challenge Reminder", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            t.Focus();
+        }
+
+        //=====Other Functions=====
+        private void displayMsg(int time, string msg)
+        {
+            //Show notification
+            //time: The time that the notification wil be shown
+            //msg: The message to show
+            TrayIcon.ShowBalloonTip(time, "Challenge Reminder", msg, ToolTipIcon.Info);
+        }
+        private void TxtMsg_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Ctrl+A shortcut key for message textbox
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                //Select all
+                TxtMsg.SelectAll();
+                //Suppress "ding"
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+    }
+}
